4a11396ef4541a719205a2b248cabad14ed3b8c2
Joseph Procopio <jprocopio@box.com>
2021-02-11 23:06:10 -0600

    adds more tokenizing

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -32,3 +32,65 @@ func TestNextToken(t *testing.T) {
 		assert.Equal(t, tok.Literal, tt.expectedLiteral, "not equal")
 	}
 }
+
+func TestMonkeyCode(t *testing.T) {
+	input := `let five = 5;
+              let ten = 10;
+              let add = fn(x, y) {
+                  x + y;
+              };
+              let result = add(five, ten);`
+
+	tests := []struct {
+		expectedType token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	actual := New(input)
+
+	for _, tt := range tests {
+
+		tok := actual.NextToken()
+
+		assert.Equal(t, tok.Type, tt.expectedType, "not equal")
+		assert.Equal(t, tok.Literal, tt.expectedLiteral, "not equal")
+	}
+}
