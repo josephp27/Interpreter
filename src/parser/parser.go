8483212a31cceeeb1abd67b9c2c6ade681161e0b
Joseph Procopio <josephp27@live.com>
2021-02-14 20:35:06 -0600

    36: formats parser

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -13,6 +13,7 @@ type Parser struct {
 }
 
 func New(l *lexer.Lexer) *Parser {
+
 	p := &Parser{l: l}
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -20,9 +21,11 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 func (p *Parser) nextToken() {
+
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 func (p *Parser) ParseProgram() *ast.Program {
+
 	return nil
 }
