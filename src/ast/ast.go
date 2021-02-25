08d1484661114463d97bbe3dc6d8da0fac87c7f3
Joseph Procopio <josephp27@live.com>
2021-02-24 22:19:08 -0600

    adds parsing bools

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -162,7 +162,7 @@ type InfixExpression struct {
 	Token    token.Token // The operator token, e.g. + Left Expression
 	Operator string
 	Right    Expression
-	Left    Expression
+	Left     Expression
 }
 
 func (oe *InfixExpression) expressionNode()      {}
@@ -179,3 +179,12 @@ func (oe *InfixExpression) String() string {
 
 	return out.String()
 }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
